c99a8dc5407d9e5f14f7aafc6760ec5d06c42c78
hidirozbey <hi.dir@hotmail.com.tr>
2014-12-11 00:20:26 +0200

    eliminasyon
    kullanici tarafindan girilen random matrix'in satirlarini eselon
    edilmeye uygun sekilde degistiriniz. yazcaginiz kodu bu baslikta branch
    yaratarak koyunuz

---
+++ eliminasyon.h
@@ -0,0 +1,6 @@
+#ifndef SOURCE1_H
+#define SOURCE1_H
+
+void echelon(double **,const int,const int);	// kullanici tarafindan girilen random matrix'in satirlarini eselon edilmeye uygun sekilde degistiriniz.
+												// ornek olarak matrisin 1. satir 1. sutun elemani 0 olmamali.(katsayilar sifirdan farkli olacak sekilde matrisi duzenleyiniz.
+#endif
