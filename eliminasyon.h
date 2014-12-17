0d8ccdbb5072df85298438d7f49225f40269ceb1
hidirozbey <hi.dir@hotmail.com.tr>
2014-12-18 00:49:23 +0200

    kapali

--- eliminasyon.h
+++ eliminasyon.h
@@ -1,6 +1,1 @@
-#ifndef SOURCE1_H
-#define SOURCE1_H
-
-void satiri_duzenle(double **,const int,const int);	// kullanici tarafindan girilen random matrix'in satirlarini eselon edilmeye uygun sekilde degistiriniz.
-												// ornek olarak matrisin 1. satir 1. sutun elemani 0 olmamali.(katsayilar sifirdan farkli olacak sekilde matrisi duzenleyiniz.
-#endif
+// dosya duzenlemeye kapanmistir...
